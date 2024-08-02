1a0832d53d00b56d7d0c938a2c00a2b2b58c4a7d
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-08-02 17:20:25 +0530

    updated

--- tourCity/src/components/Card.jsx
+++ tourCity/src/components/Card.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 function Card({id, image,info,price,name, removeTour})  {
     const[readmore,setReadmore] = useState(false);
     
+    //switch between read more show less
     const description = readmore ? info :`${info.substring(0,200)}....`;
     function readmoreHandler() {
         setReadmore(!readmore);
