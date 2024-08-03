71392d9f28bf6702becbb2af5461fd388bea03f0
Rohit Sahu <rohitrrrs1234@gmail.com>
2024-08-03 17:16:59 +0530

    top courses

---
+++ Top_Courses/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+      <nav className="bg-slate-900 py-4">
+        <h1 className="text-3xl font-bold text-center text-white">
+          Top Courses
+        </h1>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav
